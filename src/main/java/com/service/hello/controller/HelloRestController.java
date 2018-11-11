0d7b523e1bb6ad0d392f00f8dab6f2ec0e983277
Usama M. Ali <eng.usama.muhammad@gmail.com>
2018-11-11 22:28:27 +0400

    my first commit

---
+++ src/main/java/com/service/hello/controller/HelloRestController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.service.hello.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author usama
+ *
+ */
+@RestController
+public class HelloRestController {
+    @RequestMapping("/")
+    public String home() {
+	return "Hello Docker World";
+    }
+}
